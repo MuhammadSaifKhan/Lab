4bf366ffb8a72a7328d4b2affbc0d68ad75b19ee
MuhammadSaifKhan <muhammadsaifkhan2000@gmail.com>
2022-07-22 00:06:48 +0530

    new files

---
+++ DataTypes.java
@@ -0,0 +1,26 @@
+class DataTypes
+{
+	public static void main(String args[])
+		{
+		     int num1=50;
+			short myShort=500;
+			byte myByte=40;
+			long myLong=10000000L;
+			float myFloat=26.7F;
+			double myDouble=23.998888898;
+			char myChar='a';
+			boolean myBool=false;
+			System.out.println("Integer number is":"+num1);
+			System.out.println("Short Integer number is"+myShort);
+			System.out.println("Byte Integer number is"+myByte);
+			System.out.println("Long Integer number is"+myLong);
+			System.out.println(" Floating point number is"+myFloat);
+			System.out.println("Double floating point number is"+myDouble);
+			System.out.println("My first character is"+myChar);
+			if(myBool==false)
+				{
+			System.out.println("My boolean data is:"+myBool);
+				}
+			}
+
+		}
