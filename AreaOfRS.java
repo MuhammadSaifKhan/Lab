37baa565f993de80efc4b9da3621f0c661ed12d9
Muhammad Saif Khan <109592926+MuhammadSaifKhan@users.noreply.github.com>
2022-07-25 13:50:59 +0530

    Add files via upload

---
+++ AreaOfRS.java
@@ -0,0 +1,13 @@
+public class AreOfRS{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double areaOfSqure, length = 15, hieght = 25, areaOfRectangle, side = 8;
+
+		areaOfSqure = side * side;
+		areaOfRectangle = length * hieght;
+		System.out.println(areaOfSqure);
+		System.out.println(areaOfRectangle);
+	}
+
+}
